e77f03e9b72a8a3ed5ff38f662d7d1ba52037f45
pallabisushree <146589153+pallabisushree@users.noreply.github.com>
2023-10-03 02:32:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-$(document).ready(function () {
-    var count = 0;
-    $('#btn').on("click", function () {
-        var inputValue = $('.input-value').val();
-        if (inputValue != '') {
-            count++;
-            $(".to-do").append('<li class="accord"><input type="checkbox" class = "check" id="check-' + count + '"><label for="check-' + count + '"></label><span class="main-content">' + inputValue + '</span><span class="remove"><i class="material-icons">delete_forever</i></span></li>')
+document.addEventListener('DOMContentLoaded', () => {
+    const addBtn = document.getElementById('add');
+    const taskInput = document.getElementById('task');
+    const taskList = document.getElementById('task-list');
+
+    addBtn.addEventListener('click', () => {
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td>${taskText}</td>
+                <td><button class="remove-btn">Remove</button></td>
+            `;
+            taskList.appendChild(newRow);
+            taskInput.value = '';
+
+            // Attach a click event listener to the "Remove" button for the new task
+            const removeBtn = newRow.querySelector('.remove-btn');
+            removeBtn.addEventListener('click', () => {
+                newRow.remove();
+            });
         }
-        $('.input-value').val('');
-
-        $(document).on('change', 'input[type="checkbox"]', function () {
-            if ($(this).is(':checked')) {
-                $(this).closest("li").children(".remove").addClass("active");
-            } else {
-                $(this).closest("li").children(".remove").removeClass("active");
-            }
-        })
-
-        $(document).on('click', '.remove', function () {
-            $(this).parent().fadeOut(500);
-        })
-
-        $('ul').on('click', '.check', function () {
-            $(this).parent('li').toggleClass('checkin');
-        })
-    })
-})
+    });
+});
